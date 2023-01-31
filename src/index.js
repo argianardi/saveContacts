568085c2265d9a2b22179c619705e083026473ed
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-01 03:08:34 +0700

    add redux dev tool

--- src/index.js
+++ src/index.js
@@ -11,7 +11,13 @@ import thunk from "redux-thunk";
 import reducers from "./utils/redux/reducers";
 import { Provider } from "react-redux";
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
