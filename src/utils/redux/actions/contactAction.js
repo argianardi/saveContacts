5bcdc32a776a8943adaa1c6af4e937060f759b9c
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-01 02:42:49 +0700

    add post request action

--- src/utils/redux/actions/contactAction.js
+++ src/utils/redux/actions/contactAction.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const GET_LIST_CONTACT = "GET_LIST_CONTACT";
+export const ADD_CONTACT = "ADD_CONTACT";
 
 export const getListContact = () => {
-  console.log("2. masuk action");
   return (dispatch) => {
     // Loading
     dispatch({
@@ -20,7 +20,6 @@ export const getListContact = () => {
       .get("http://localhost:2023/contacts")
       .then((response) => {
         // berhasil get api
-        console.log("3. Berhasil dapat data: ", response);
         dispatch({
           type: GET_LIST_CONTACT,
           payload: {
@@ -32,9 +31,51 @@ export const getListContact = () => {
       })
       .catch((error) => {
         // gagal get api
-        console.log("3. Gagal dapat data:", error.message);
         dispatch({
           type: GET_LIST_CONTACT,
+          payload: {
+            loading: false,
+            data: false,
+            errorMessage: error.message,
+          },
+        });
+      });
+  };
+};
+
+export const addContact = (data) => {
+  console.log("2. masuk action");
+  return (dispatch) => {
+    // Loading
+    dispatch({
+      type: ADD_CONTACT,
+      payload: {
+        loading: true,
+        data: false,
+        errorMessage: false,
+      },
+    });
+
+    // post API
+    axios
+      .post("http://localhost:2023/contacts", data)
+      .then((response) => {
+        // berhasil post api
+        console.log("3. Berhasil dapat data: ", response);
+        dispatch({
+          type: ADD_CONTACT,
+          payload: {
+            loading: false,
+            data: response.data,
+            errorMessage: false,
+          },
+        });
+      })
+      .catch((error) => {
+        // gagal post api
+        console.log("3. Gagal dapat data:", error.message);
+        dispatch({
+          type: ADD_CONTACT,
           payload: {
             loading: false,
             data: false,
