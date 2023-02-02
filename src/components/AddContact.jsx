9a6912a558d974265f703b6ee9c5db92f5803669
Argian Ardi Prasetya <argianardi1435@gmail.com>
2023-02-02 07:22:44 +0700

    add put request

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -3,29 +3,56 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   addContact,
   getListContact,
+  updateContact,
 } from "../utils/redux/actions/contactAction";
 
 const AddContact = () => {
   const dispatch = useDispatch();
-  const { addContactResult } = useSelector((state) => state.ContactReducer);
+  const { addContactResult, detailContactResult, updateContactResult } =
+    useSelector((state) => state.ContactReducer);
 
   const [name, setName] = useState("");
   const [nohp, setNohp] = useState("");
+  const [id, setId] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addContact({ name, nohp }));
+    if (id) {
+      // update Contact
+      dispatch(updateContact({ id, name, nohp }));
+    } else {
+      dispatch(addContact({ name, nohp }));
+    }
   };
 
   useEffect(() => {
     if (addContactResult) {
       dispatch(getListContact());
+      setName("");
+      setNohp("");
     }
   }, [addContactResult, dispatch]);
 
+  useEffect(() => {
+    if (detailContactResult) {
+      setName(detailContactResult.name);
+      setNohp(detailContactResult.nohp);
+      setId(detailContactResult.id);
+    }
+  }, [detailContactResult, dispatch]);
+
+  useEffect(() => {
+    if (updateContactResult) {
+      dispatch(getListContact());
+      setName("");
+      setNohp("");
+      setId("");
+    }
+  }, [updateContactResult, dispatch]);
+
   return (
     <div>
-      <h4>Add Contact</h4>
+      <h4>{id ? "Edit Contact" : "Add Contact"}</h4>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
